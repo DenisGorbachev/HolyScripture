e5538fb9794b5330232d2f0b372e16a3fdb6cb26
Denis Gorbachev <Denis.Gorbachev@faster-than-wind.ru>
2012-12-24 15:27:37 +0400

    Fixed validatePartner

--- Islam/Person.php
+++ Islam/Person.php
@@ -40,7 +40,7 @@ class Person extends CommonPerson
 
     public function validatePartner(Person $partner)
     {
-        if ($partner->getGender() == $partner->getGender()) {
+        if ($this->getGender() == $partner->getGender()) {
             // Leviticus 18:22: "Do not have sexual relations with a man as one does with a woman; that is detestable"
             throw new \Exception('Homosexuality is not allowed');
         }
